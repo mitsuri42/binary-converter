c2195ff7be4ba61bf640a452a2f5dca6a57827fc
Dmitriy Pysarchuk <dmit.pysarchuk@gmail.com>
2024-03-01 18:36:04 +0200

    Add while loop to decimalToBinary function

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-  
+  const inputs = [];
+  const quotients = [];
+  const remainders = [];
+
+  while (input > 0) {
+
+
+    input = 0;
+  }
 };
 
 const checkUserInput = () => {
