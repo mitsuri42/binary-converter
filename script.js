1c618a25a35ff8d347934f6d3b50129c0c5bd463
Dmitriy Pysarchuk <dmit.pysarchuk@gmail.com>
2024-03-06 16:25:56 +0200

    Modify chechUserInput function

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const checkUserInput = () => {
     return;
   }
 
-  decimalToBinary(parseInt(numberInput.value));
+  result.textContent = decimalToBinary(parseInt(numberInput.value));
   numberInput.value = "";
 };
 
